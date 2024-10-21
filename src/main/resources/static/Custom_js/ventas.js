aff6b5184b55614447f1ecd30a0c41e08d425bd2
GimblerJara <jaragamarra1095@gmail.com>
2024-10-20 23:18:49 -0500

    Actualizacion

--- src/main/resources/static/Custom_js/ventas.js
+++ src/main/resources/static/Custom_js/ventas.js
@@ -17,3 +17,20 @@ document.addEventListener('DOMContentLoaded', function() {
 	// Valida si hay productos seleccionados en la tabla al cargar la página
 	validateForm();
 });
+
+
+document.getElementById('productoSelect').addEventListener('change', function() {
+	const productoId = this.value; // Obtener el ID del producto seleccionado
+	if (productoId) {
+		fetch(`/productos/stock/${productoId}`)
+			.then(response => response.json())
+			.then(data => {
+				document.getElementById('stockDisponible').value = data.stockActual; // Actualizar el campo de stock disponible
+			})
+			.catch(error => console.error('Error:', error));
+	} else {
+		document.getElementById('stockDisponible').value = ''; // Limpiar el campo si no se selecciona un producto
+	}
+});
+
+
